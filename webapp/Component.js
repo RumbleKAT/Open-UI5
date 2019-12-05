36bcfd8b2667c1d25054b1f24440fd6c5f48cf74
RumbleKAT <reki318@naver.com>
2019-12-05 23:46:57 +0900

    add lecture 1

---
+++ webapp/Component.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.demo.nav.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+		init : function(){
+			UIComponent.prototype.init.apply(this,arguments);
+			this.getRouter().initialize();
+		}
+
+	});
+
+});
