82b511f52e1ee290fbfe25a5c64d92de82216748
RumbleKAT <reki318@naver.com>
2019-12-08 21:51:09 +0900

    add remove hash for notFound

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -5,6 +5,12 @@ sap.ui.define([
     return BaseController.extend("sap.ui.demo.nav.controller.Home",{
         onInit: function(){
             console.log("initialize Home page..");
+        },
+        onDisplayNotFound : function(oEvent){
+            // this.getRouter().getTargets().display("notFound"); //getTargets의 객체에 대한 참조를 가져온다.
+            this.getRouter().getTargets().display("notFound",{
+                fromTarget : "home"
+            });
         }
     });
 })
--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -5,6 +5,23 @@ sap.ui.define([
     return BaseController.extend("sap.ui.demo.nav.controller.NotFound",{
         onInit: function(){
             console.log("page not found...");
+            let oRouter, oTarget;
+            oRouter = this.getRouter();
+            oTarget = oRouter.getTarget("notFound");
+            oTarget.attachDisplay(function(oEvent){
+                this._oData = oEvent.getParameter("data");
+            },this);
         }//bypassed 속성을 이용하여, url 대처 , sap.m.MessagePage 컨트롤 사용하여 라우팅 오류 메시지 표시
+        ,onNavBack : function(oEvent){
+            //hash값의 변화없이 notFound 페이지로 이동
+            console.log(this._oData);
+            if(this._oData && this._oData.fromTarget){
+                this.getRouter().getTargets().display(this._oData.fromTarget);
+                delete this._oData.fromTarget;
+                return;
+            }
+
+            BaseController.prototype.onNavBack.apply(this.arguments);
+        }
     });
 })
