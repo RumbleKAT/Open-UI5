058859f3d6b430c7d4e02e985d9cfbb7c2ea7d78
RumbleKAT <reki318@naver.com>
2019-12-08 21:10:24 +0900

    add baseController.js to replace Controller.js

--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -1,11 +1,10 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],function(Controller){
+    "sap/ui/demo/nav/controller/BaseController"
+],function(BaseController){
     "use strict"
-    return Controller.extend("sap.ui.demo.nav.controller.NotFound",{
+    return BaseController.extend("sap.ui.demo.nav.controller.NotFound",{
         onInit: function(){
             console.log("page not found...");
         }//bypassed 속성을 이용하여, url 대처 , sap.m.MessagePage 컨트롤 사용하여 라우팅 오류 메시지 표시
-        
     });
 })
